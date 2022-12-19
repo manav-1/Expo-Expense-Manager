d481ae778472d61b83a2cfb9a260614ddbafc006
Manav Arora <Manav81101@gmail.com>
2022-12-19 22:45:08 +0530

    Expo eas addition

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const ExpenseClient = axios.create({
-  baseURL: 'https://backend.loca.lt',
+  baseURL: 'https://expense-manager-nest-js-production.up.railway.app/',
   timeout: 1000,
 });
 
